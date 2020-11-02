b2fc79c23ebdb0647060d3fc7b52ac3a5ec53484
shhthome <18236786737@139.com>
2020-11-02 23:19:08 +0800

    spring-boot-项目学习第一次提交

---
+++ src/main/java/com/hwf/springboothello002/SpringBootHello002Application.java
@@ -0,0 +1,14 @@
+package com.hwf.springboothello002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHello002Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootHello002Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hwf/springboothello002/SpringBootHello002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwf.springboothello002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHello002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
